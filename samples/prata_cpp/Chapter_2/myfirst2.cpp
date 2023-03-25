cc03540fd1641c820ffce2a3283857516deb0b75
jenyck <evgengaziin@gmail.com>
2023-03-25 12:00:05 +0300

    add samples

---
+++ samples/prata_cpp/Chapter_2/myfirst2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>                           // a PREPROCESSOR directive
+
+int main()                                    // function header
+{                                             // start of function body
+    printf("Come up and C++ me some time.");  // message
+    printf("\n");                             // start a new line
+    printf("You won't regret it!");   // more output
+// If the output window closes before you can read it,
+// add the following code:
+    return 0;                                 // terminate main()
+}                                             // end of function body
